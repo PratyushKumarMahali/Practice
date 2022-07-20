4a33fa9b59a0a4a9c325e937ad237334fa1feecd
Pratyush Kumar Mahali <83828190+PratyushKumarMahali@users.noreply.github.com>
2022-07-20 17:52:17 +0530

    Add files via upload

---
+++ list_dict.py
@@ -0,0 +1,12 @@
+list1 = [5, 10, 15]
+sum1 = sum(list1)
+length1 = len(list1)
+average1 = sum1 / length1
+print(average1)
+
+dict1 = {"Allen":5, "Solly":10, "Vice":15}
+sum2 = sum(dict1.values())
+length2 = len(dict1)
+average2 = sum2 / length2
+print(average2)
+print(dict1.keys())
